bc078b7235bd2445bc59e53d2cef88410220cedd
mohmed97 <m.ahmed9788@gmail.com>
2021-12-26 22:03:22 +0200

    Version 2

--- app/app.py
+++ app/app.py
@@ -4,13 +4,24 @@ from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 import numpy as np
 import os 
+import cv2
+import tensorflow as tf 
+from tensorflow import keras
 
 app = Flask(__name__)
    
 @app.route('/',methods=['GET', 'POST'])
 def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET', 'POST'])
+def predict():
    return render_template('home.html')
-   
+
+@app.route('/predict',methods=['GET', 'POST'])
+def predict():
+   return render_template('about.html')
+ 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -18,8 +29,13 @@ def upload_file():
         if file :
             app.config['UPLOAD_FOLDER']=os.path.dirname(__file__)
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return render_template('index.html')
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded/images/'+filename)
+            file.save(filepath)
+            myImg = cv2.resize(cv2.imread(filepath), (224,224))
+            myModel=tf.keras.models.load_model('effnet.h5')
+            # result = myModel.predict(myImg)
+            # ,predict=result
+        return render_template('result.html')
     
 
 if __name__ == '__main__':
