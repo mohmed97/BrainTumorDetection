fec8e207a626780e9d64073e0d170f494ecb76fd
mohmed97 <m.ahmed9788@gmail.com>
2022-01-01 16:52:22 +0200

    Version 4

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from werkzeug.datastructures import  FileStorage
 import numpy as np
 import os 
 import cv2
-from PIL import Image
+# from PIL import Image
 import tensorflow as tf
 from tensorflow import keras
 
@@ -25,6 +25,10 @@ def predict():
 def about():
    return render_template('about.html')
 
+@app.route('/contact',methods=['GET', 'POST'])
+def contact():
+   return render_template('contact.html')
+
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -32,7 +36,7 @@ def upload_file():
         if file :
             app.config['UPLOAD_FOLDER']=os.path.dirname(__file__)
             filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded/images/'+filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], 'static/images/'+filename)
             file.save(filepath)
 
             image = cv2.imread(filepath)
@@ -40,7 +44,7 @@ def upload_file():
             image = cv2.resize(image, (224, 224))
             image = img_to_array(image)
             image = image.reshape(1,224,224,3)
-            classes = ['glioma_tumor', 'no_tumor', 'meningioma_tumor', 'pituitary_tumor']
+            classes = ['Glioma Tumor', 'No Tumor', 'Meningioma Tumor', 'Pituitary Tumor']
             # im = cv2.imread(filepath)
             # # myImg = np.array(im.resize(224,224))
             # myImg = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
@@ -49,8 +53,8 @@ def upload_file():
             myModel=tf.keras.models.load_model('effnet.h5')
             result = myModel.predict(image)
             output = classes[np.where(result == np.amax(result))[1][0]]
-        # , predict = result
-        return render_template('result.html', predict = output)
+            filePath = "images/"+filename
+        return render_template('result.html', predict = output,filePath = filePath)
     
 
 if __name__ == '__main__':
